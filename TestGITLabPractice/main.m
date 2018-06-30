bc2cc2a5cd0e55de6f4087cf647ef308fb2201ef
Vamsi <vamsi@Vamsis-Mac.local>
2018-06-30 10:56:44 +0530

    Initial Commit

---
+++ TestGITLabPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGITLabPractice
+//
+//  Created by Vamsi on 09/04/1940 Saka.
+//  Copyright © 1940 Vamsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
